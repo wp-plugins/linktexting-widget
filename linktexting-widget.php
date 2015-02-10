bf4d837587cace473bdac23bc5aaed6d8b96a06c
LinkTexting <LinkTexting@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 23:59:58 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/linktexting-widget/trunk@1086802 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ linktexting-widget.php
@@ -0,0 +1,59 @@
+<?php
+/* 
+Plugin Name: LinkTexting Widget
+Plugin URI: https://www.linktexting.com/ 
+Version: 1.0
+Author: Brian Clark
+Description: A plugin for creating a text-to-dowload form for mobile apps 
+*/  
+
+function linktexting_shortcode( $atts ) {
+
+       // Attributes
+       extract( shortcode_atts(
+              array(
+                     'linkid' => 'link_uuid_here',
+                     'button_color' => '#aaa',
+                     'button_color_hover' => '#666',
+              ), $atts )
+       );
+
+       // Code
+return        '<script type="text/javascript">'.
+              '  var utilsScript = "'.plugins_url('/assets/js/utils.js', __FILE__).'"'.
+              '</script>'.
+              '<style type="text/css">'.
+              '  .linkTextingButton_oq3j39q0 {'.
+              '    background-color: '.$atts['button_color'].';'.
+              '    box-shadow: none;'.
+              '    text-shadow: none;'.
+              '  }'.
+              '  .linkTextingButton_oq3j39q0:hover {'.
+              '    background-color: '.$atts['button_color_hover'].';'.
+              '    box-shadow: none;'.
+              '    text-shadow: none;'.
+              '  }'.
+              '</style>'.
+              '<div class="linkTexting_oq3j39q0" style="margin:0;">'.
+              '  <div class="linkTextingInner_oq3j39q0" style="margin:0;">'.
+              '    <input class="linkTextingInput_oq3j39q0" type="tel" id="numberToText_linkTexting_oq3j39q0_'.$atts['linkid'].'"></input>'.
+              '    <button class="linkTextingButton_oq3j39q0" type="button" onclick="sendLink_linkTexting_oq3j39q0(\''.$atts['linkid'].'\')" id="sendButton_linkTexting_oq3j39q0_'.$atts['linkid'].'">Text me a link</button>'.
+              '    <div class="linkTextingError_oq3j39q0" id="linkTextingError_oq3j39q0_'.$atts['linkid'].'" style="display:none;"></div>'.
+              '  </div>'.
+              '</div>';
+  
+}
+
+function addStyle() {
+       wp_enqueue_style( 'style', plugins_url('/assets/css/style.css', __FILE__) );
+}
+
+function addScripts() {
+       wp_enqueue_script( 'main', plugins_url('/assets/js/main.js', __FILE__), array( 'jquery' ) );
+}
+
+add_action('wp_enqueue_scripts', 'addStyle');
+add_action('wp_footer', 'addScripts');
+add_shortcode( 'linktexting', 'linktexting_shortcode' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Name ===
+Contributors: LinkTexting
+Donate link: https://www.linktexting.com/
+Tags: LinkTexting, Link, Text, SMS, download, mobile, app
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Install a text-to-download widget for your wordpress site
+
+== Description ==
+
+LinkTexting is a text-to-download service for your users to text themselves a download link for your mobile app.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `linktexting-wdiget.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Sign up at LinkTexting.com and create a link
+4. Get the Link ID
+5. Use and customize our widget
+
+[linktexting linkid="Your_Link_ID" button_color="#aaa" button_color_hover="#666"]
+
+== Frequently Asked Questions ==
+
+= My widget says Invalid Domain =
+
+Make sure to have a comma separated list of every domain you use
+(i.e https://wordpress.com, http://subdomain.wordpress.com)
+
+== Screenshots ==
+
+1. The active and properly working LinkTexting widget
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial release
