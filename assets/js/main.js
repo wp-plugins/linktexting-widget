332fdce3015c8b0e51614e2d224005df2c3b8027
LinkTexting <LinkTexting@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-25 04:24:23 +0000

    tagging version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/linktexting-widget/tags/1.4@1120167 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ var ipinfoToken_oq3j39q0 = "";
 var preferredCountries_oq3j39q0 = ["us", "gb"];
 var onlyCountries_oq3j39q0 = ["af","al","dz","as","ad","ao","ai","ag","ar","am","aw","au","at","az","bs","bh","bd","bb","by","be","bz","bj","bm","bt","bo","ba","bw","br","io","vg","bn","bg","bf","bi","kh","cm","ca","cv","bq","ky","cf","td","cl","cn","co","km","cd","cg","ck","cr","ci","hr","cu","cw","cy","cz","dk","dj","dm","do","ec","eg","sv","gq","er","ee","et","fk","fo","fj","fi","fr","gf","pf","ga","gm","ge","de","gh","gi","gr","gl","gd","gp","gu","gt","gn","gw","gy","ht","hn","hk","hu","is","in","id","ir","iq","ie","il","it","jm","jp","jo","kz","ke","ki","kw","kg","la","lv","lb","ls","lr","ly","li","lt","lu","mo","mk","mg","mw","my","mv","ml","mt","mh","mq","mr","mu","mx","fm","md","mc","mn","me","ms","ma","mz","mm","na","nr","np","nl","nc","nz","ni","ne","ng","nu","nf","kp","mp","no","om","pk","pw","ps","pa","pg","py","pe","ph","pl","pt","pr","qa","re","ro","ru","rw","bl","sh","kn","lc","mf","pm","vc","ws","sm","st","sa","sn","rs","sc","sl","sg","sx","sk","si","sb","so","za","kr","ss","es","lk","sd","sr","sz","se","ch","sy","tw","tj","tz","th","tl","tg","tk","to","tt","tn","tr","tm","tc","tv","vi","ug","ua","ae","gb","us","uy","uz","vu","va","ve","vn","wf","ye","zm","zw"];
 
-loadIntlInput_oq3j39q0();
 var sendLink_linkTexting_oq3j39q0 = function(linkId) {
 	document.getElementById('linkTextingError_oq3j39q0_'+linkId).style.display = 'none';
 	document.getElementById('sendButton_linkTexting_oq3j39q0_'+linkId).disabled = true;
@@ -42,6 +41,6 @@ function loadIntlInput_oq3j39q0() {
 		ipinfoToken: typeof ipinfoToken_oq3j39q0 === "undefined" ? "" : ipinfoToken_oq3j39q0,
 		onlyCountries: typeof onlyCountries_oq3j39q0 === "undefined" ? undefined : onlyCountries_oq3j39q0,
 		nationalMode: true,
-		utilsScript: utilsScript
+		utilsScript: "//d3q6uu7asevdsg.cloudfront.net/intl-tel-input/lib/libphonenumber/build/utils.js"
 	});
 }
