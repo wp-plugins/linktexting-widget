f35e00995e18c785f30c82e1219863a6ce74adb4
LinkTexting <LinkTexting@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-30 01:08:13 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/linktexting-widget/tags/2.0@1149702 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/linktexting-widget.php
@@ -0,0 +1,64 @@
+<?php
+/* 
+Plugin Name: LinkTexting Widget
+Plugin URI: https://www.linktexting.com/ 
+Version: 2.0
+Author: Brian Clark
+Description: A plugin for creating a text-to-dowload form for mobile apps 
+*/  
+
+function linktexting_shortcode( $atts ) {
+
+       // Attributes
+       extract( shortcode_atts(
+              array(
+                     'linkid' => 'link_uuid_here',
+                     'button_color' => '#aaa',
+                     'button_color_hover' => '#666',
+              ), $atts )
+       );
+
+       // Code
+return        '<style type="text/css">'.
+              '  .linkTextingButton_oq3j39q0 {'.
+              '    background-color: '.$atts['button_color'].';'.
+              '    background: '.$atts['button_color'].';'.
+              '    box-shadow: none;'.
+              '    text-shadow: none;'.
+              '  }'.
+              '  .linkTextingButton_oq3j39q0:hover {'.
+              '    background-color: '.$atts['button_color_hover'].';'.
+              '    background: '.$atts['button_color_hover'].';'.
+              '    box-shadow: none;'.
+              '    text-shadow: none;'.
+              '  }'.
+              '</style>'.
+              '<div class="linkTextingWidget" style="margin:0;">'.
+              '  <div class="linkTextingInner" style="margin:0;">'.
+              '    <input type="hidden" class="linkID" value="'.$atts['linkid'].'">'.
+              '    <input class="linkTextingInput" type="tel"></input>'.
+              '    <button class="linkTextingButton" type="button">Text me a link</button>'.
+              '    <div class="linkTextingError" style="display:none;"></div>'.
+              '  </div>'.
+              '</div>';
+}
+
+function addStyle() {
+       wp_enqueue_style( 'style', plugins_url('/assets/css/style.css', __FILE__) );
+}
+
+function addScripts() {
+       wp_register_script( 'utilsScript', plugins_url('/assets/js/utils.js', __FILE__) );
+       wp_register_script( 'main', plugins_url('/assets/js/main.js', __FILE__) );
+       $translation_array = array(
+              'plugins_url' => __( plugins_url(), 'plugin-domain' )
+       );
+       wp_localize_script( 'main', 'utils', $translation_array );
+       wp_enqueue_script( 'main', plugins_url('/assets/js/main.js', __FILE__), array( 'jquery', 'utilsScript' ) );
+}
+
+add_action('wp_enqueue_scripts', 'addStyle');
+add_action('wp_footer', 'addScripts');
+add_shortcode('linktexting', 'linktexting_shortcode');
+
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,59 @@
+=== Plugin Name ===
+Contributors: LinkTexting
+Donate link: https://www.linktexting.com/
+Tags: LinkTexting, Link, Text, SMS, download, mobile, app
+Requires at least: 3.0
+Tested up to: 4.2.1
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Install a text-to-download widget for your wordpress site
+
+== Description ==
+
+LinkTexting is a text-to-download service for your users to text themselves a download link for your mobile app.
+
+== Installation ==
+
+1. Install 'linktexting-widget.zip' on the 'Plugins' menu in Wordpress then activate it
+2. Sign up at LinkTexting.com and create a link
+3. Get the Link ID
+4. Use and customize our widget
+
+[linktexting linkid="Your_Link_ID" button_color="#aaa" button_color_hover="#666"]
+
+== Frequently Asked Questions ==
+
+= My widget says Invalid Domain =
+
+Make sure to have a comma separated list of every domain you use
+(i.e https://wordpress.com, http://subdomain.wordpress.com)
+
+== Screenshots ==
+
+1. The active and properly working LinkTexting widget
+2. LinkTexting on Hinge
+3. LinkTexting on Meerkat
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+= 1.3 =
+* Bug fixes
+
+= 2.0 = 
+* Support multiple widgets on one page
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial release
+
+= 1.3 =
+Bug fixes
+
+= 2.0 = 
+* Support multiple widgets on one page
